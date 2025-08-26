1185045fc30ca37ab225aa627254f16438d4961b
ahmed <ahmedaoudah1@gmail.com>
2025-08-27 00:39:49 +0300

    qw

--- resources/views/Sumbook/client/Sumbook.blade.php
+++ resources/views/Sumbook/client/Sumbook.blade.php
@@ -21,7 +21,7 @@
 
                         <!-- زر الاستعراض -->
                         <a href="{{ route('sumbook.show', $sumbook->id) }}" target="_blank"
-                           class="inline-block text-center bg-yellow-600 text-white px-4 py-2 r">
+                           class=" bg-yellow-600 ">
                             استعراض
                         </a>
 
