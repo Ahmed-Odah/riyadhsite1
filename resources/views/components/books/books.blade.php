d4dc72bb66f23a109d82c2d8beb19afc5abf1170
ahmed <ahmedaoudah1@gmail.com>
2025-08-27 16:51:34 +0300

    qw

--- resources/views/components/books/books.blade.php
+++ resources/views/components/books/books.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-gray-50 min-h-screen py-20 px-6 sm:px-12 lg:px-24">
-    <h1 class="text-4xl font-extrabold text-center text-gray-900 mb-14 mt-20">مؤلفاتي٫</h1>
+    <h1 class="text-4xl font-extrabold text-center text-gray-900 mb-14 mt-20">مؤلفاتي</h1>
 
     <div class="max-w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-4 lg:px-12 xl:px-20">
         @foreach($books as $book)
@@ -22,10 +22,7 @@
                     <p class="text-gray-600 text-sm mb-6 line-clamp-5">
                         {{ $book->description }}
                     </p>
-                    <a href="{{ route('book.read', $book->id) }}"
-                       class="mt-auto bg-indigo-900 hover:bg-indigo-600 text-white text-sm font-medium py-2 rounded-md text-center transition-colors duration-300">
-                        عرض الكتاب
-                    </a>
+                    <!-- زر عرض الكتاب تمت إزالته -->
                 </div>
             </div>
         @endforeach
