522c6be96ca45cd4be25dab9df8244bfc723bab6
ahmed <ahmedaoudah1@gmail.com>
2025-08-29 14:47:23 +0300

    qw

--- resources/views/coins/client/show.blade.php
+++ resources/views/coins/client/show.blade.php
@@ -4,23 +4,21 @@
     <div x-data="{ showBack: false, modalOpen: false, modalBack: false }" class="bg-gray-50 min-h-screen py-24 px-4 sm:px-6 lg:px-20 flex flex-col items-center space-y-16">
 
         {{-- البطاقة الصغيرة --}}
-        <div class="w-80 aspect-[85/54] perspective cursor-pointer" @click="modalOpen = true">
-            <div class="flip-card-inner relative w-full h-full transition-transform duration-500"
-                 :class="{'rotate-y-180': showBack}"
-                 @click.stop="showBack = !showBack">
+        <div class="max-w-4xl w-full perspective cursor-pointer" @click="modalOpen = true">
+            <div class="flip-card-inner relative w-full h-96 transition-transform duration-500" :class="{'rotate-y-180': showBack}" @click.stop="showBack = !showBack">
 
                 {{-- الوجه --}}
                 @if($coin->image)
                     <img src="{{ asset('public/storage/' . $coin->image) }}"
                          alt="{{ $coin->title }}"
-                         class="flip-card-front absolute w-full h-full object-contain rounded-xl backface-hidden shadow-lg bg-white">
+                         class="flip-card-front absolute w-full h-full object-cover rounded-3xl backface-hidden shadow-xl">
                 @endif
 
                 {{-- الظهر --}}
                 @if($coin->back_image)
                     <img src="{{ asset('public/storage/' . $coin->back_image) }}"
                          alt="{{ $coin->title }} - Back"
-                         class="flip-card-back absolute w-full h-full object-contain rounded-xl backface-hidden rotate-y-180 shadow-lg bg-white">
+                         class="flip-card-back absolute w-full h-full object-cover rounded-3xl backface-hidden rotate-y-180 shadow-xl">
                 @endif
             </div>
         </div>
