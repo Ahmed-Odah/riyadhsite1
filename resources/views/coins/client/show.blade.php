f0f87c92ebf7a2c5a461c3ca26badf2e38b77699
ahmed <ahmedaoudah1@gmail.com>
2025-08-29 14:45:37 +0300

    qw

--- resources/views/coins/client/show.blade.php
+++ resources/views/coins/client/show.blade.php
@@ -1,29 +1,26 @@
-@extends('layout.master')
-@section('content')
+{{-- البطاقة الصغيرة --}}
+<div class="max-w-sm w-full perspective cursor-pointer" @click="modalOpen = true">
+    <div class="flip-card-inner relative w-full h-56 transition-transform duration-500"
+         :class="{'rotate-y-180': showBack}"
+         @click.stop="showBack = !showBack">
 
-    <div x-data="{ showBack: false, modalOpen: false, modalBack: false }" class="bg-gray-50 min-h-screen py-24 px-4 sm:px-6 lg:px-20 flex flex-col items-center space-y-16">
+        {{-- الوجه --}}
+        @if($coin->image)
+            <img src="{{ asset('public/storage/' . $coin->image) }}"
+                 alt="{{ $coin->title }}"
+                 class="flip-card-front absolute w-full h-full object-cover rounded-xl backface-hidden shadow-lg">
+        @endif
 
-        {{-- البطاقة الصغيرة --}}
-        <div class="max-w-4xl w-full perspective cursor-pointer" @click="modalOpen = true">
-            <div class="flip-card-inner relative w-full h-96 transition-transform duration-500" :class="{'rotate-y-180': showBack}" @click.stop="showBack = !showBack">
+        {{-- الظهر --}}
+        @if($coin->back_image)
+            <img src="{{ asset('public/storage/' . $coin->back_image) }}"
+                 alt="{{ $coin->title }} - Back"
+                 class="flip-card-back absolute w-full h-full object-cover rounded-xl backface-hidden rotate-y-180 shadow-lg">
+        @endif
+    </div>
+</div>
 
-                {{-- الوجه --}}
-                @if($coin->image)
-                    <img src="{{ asset('public/storage/' . $coin->image) }}"
-                         alt="{{ $coin->title }}"
-                         class="flip-card-front absolute w-full h-full object-cover rounded-3xl backface-hidden shadow-xl">
-                @endif
-
-                {{-- الظهر --}}
-                @if($coin->back_image)
-                    <img src="{{ asset('public/storage/' . $coin->back_image) }}"
-                         alt="{{ $coin->title }} - Back"
-                         class="flip-card-back absolute w-full h-full object-cover rounded-3xl backface-hidden rotate-y-180 shadow-xl">
-                @endif
-            </div>
-        </div>
-
-        {{-- العنوان والوصف والدولة --}}
+{{-- العنوان والوصف والدولة --}}
         <h1 class="text-4xl md:text-5xl font-extrabold text-gray-900 mt-6 mb-4 text-center">{{ $coin->title }}</h1>
 
         @if($coin->description)
