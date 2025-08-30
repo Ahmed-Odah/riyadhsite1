0dff22e47a47455f7073a1fefb39ab122362ca75
ahmed <ahmedaoudah1@gmail.com>
2025-08-30 15:25:16 +0300

    qw

--- resources/views/coins/client/show.blade.php
+++ resources/views/coins/client/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.master')
 @section('content')
 
-    <div x-data="{ modalOpen: false, selectedImage: null }"
+    <div x-data="{ modalOpen: false, selectedCoin: null }"
          class="bg-gray-50 min-h-screen py-24 px-4 sm:px-6 lg:px-20 flex flex-col items-center space-y-16">
 
         {{-- البطاقة الرئيسية --}}
-        <div class="max-w-4xl w-full perspective cursor-pointer"
-             @click="modalOpen = true; selectedImage = '{{ asset('public/storage/' . $coin->image) }}'">
+        <div class="max-w-4xl w-full cursor-pointer"
+             @click="modalOpen = true; selectedCoin = @js($coin)">
             <div class="relative w-full h-96 transition-transform duration-500">
                 @if($coin->image)
                     <img src="{{ asset('public/storage/' . $coin->image) }}"
@@ -36,7 +36,7 @@
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     @foreach($coin->relatedCoins as $related)
                         <div class="cursor-pointer"
-                             @click="modalOpen = true; selectedImage = '{{ asset('public/storage/' . $related->image) }}'">
+                             @click="modalOpen = true; selectedCoin = @js($related)">
                             <div class="relative w-full h-64 transition-transform duration-500">
                                 @if($related->image)
                                     <img src="{{ asset('public/storage/' . $related->image) }}"
@@ -53,18 +53,42 @@
             </div>
         @endif
 
-        {{-- مودال عرض الصورة --}}
+        {{-- مودال عرض الصورة مع قلبها --}}
         <div x-show="modalOpen" x-transition.opacity
              class="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50 p-4"
              @click="modalOpen = false">
-            <div @click.stop class="relative max-w-[90vw] max-h-[90vh] flex items-center justify-center">
-                <img :src="selectedImage"
-                     class="max-h-[90vh] max-w-[90vw] object-contain rounded-2xl shadow-2xl" />
+
+            <div @click.stop x-data="{ modalBack: false }" class="relative max-w-[90vw] max-h-[90vh] flex items-center justify-center">
+
+                <div class="relative w-full h-full transition-transform duration-500"
+                     :class="{'rotate-y-180': modalBack}"
+                     @click="modalBack = !modalBack">
+
+                    {{-- الوجه --}}
+                    <template x-if="!modalBack">
+                        <img :src="'/storage/' + selectedCoin?.image"
+                             class="absolute max-h-[90vh] max-w-[90vw] object-contain rounded-2xl shadow-2xl">
+                    </template>
+
+                    {{-- الظهر --}}
+                    <template x-if="modalBack">
+                        <img :src="'/storage/' + selectedCoin?.back_image ?? selectedCoin?.image"
+                             class="absolute max-h-[90vh] max-w-[90vw] object-contain rounded-2xl shadow-2xl">
+                    </template>
+
+                </div>
+
+                {{-- زر إغلاق --}}
                 <button @click="modalOpen = false"
                         class="absolute top-3 right-3 text-white text-4xl font-extrabold hover:text-gray-300">&times;</button>
             </div>
         </div>
 
     </div>
+
+    {{-- CSS --}}
+    <style>
+        .rotate-y-180 { transform: rotateY(180deg); transition: transform 0.5s; }
+    </style>
 
 @endsection
