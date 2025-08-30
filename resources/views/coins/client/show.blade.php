64af3da38fdeb962574a4ed59c2c97526a27d817
ahmed <ahmedaoudah1@gmail.com>
2025-08-30 15:18:59 +0300

    qw

--- resources/views/coins/client/show.blade.php
+++ resources/views/coins/client/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.master')
 @section('content')
 
-    <div x-data="{ showBack: false, modalOpen: false, modalBack: false, selectedCoin: null }"
+    <div x-data="{ showBack: false, modalOpen: false, selectedImage: null }"
          class="bg-gray-50 min-h-screen py-24 px-4 sm:px-6 lg:px-20 flex flex-col items-center space-y-16">
 
         {{-- البطاقة الرئيسية --}}
         <div class="max-w-4xl w-full perspective cursor-pointer"
-             @click="modalOpen = true; selectedCoin = @js($coin)">
+             @click="modalOpen = true; selectedImage = '{{ asset('storage/' . $coin->image) }}'">
             <div class="flip-card-inner relative w-full h-96 transition-transform duration-500"
                  :class="{'rotate-y-180': showBack}"
                  @click.stop="showBack = !showBack">
@@ -22,7 +22,8 @@
                 @if($coin->back_image)
                     <img src="{{ asset('public/storage/' . $coin->back_image) }}"
                          alt="{{ $coin->title }} - Back"
-                         class="flip-card-back absolute w-full h-full object-cover rounded-3xl backface-hidden rotate-y-180 shadow-xl">
+                         class="flip-card-back absolute w-full h-full object-cover rounded-3xl backface-hidden rotate-y-180 shadow-xl"
+                         @click="modalOpen = true; selectedImage = '{{ asset('storage/' . $coin->back_image) }}'">
                 @endif
             </div>
         </div>
@@ -47,7 +48,7 @@
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     @foreach($coin->relatedCoins as $related)
                         <div class="perspective cursor-pointer"
-                             @click="modalOpen = true; selectedCoin = @js($related)">
+                             @click="modalOpen = true; selectedImage = '{{ asset('storage/' . $related->image) }}'">
                             <div class="flip-card-inner relative w-full h-64 transition-transform duration-500"
                                  x-data="{ showBack: false }"
                                  :class="{'rotate-y-180': showBack}"
@@ -60,7 +61,8 @@
                                 @if(isset($related->back_image))
                                     <img src="{{ asset('public/storage/' . $related->back_image) }}"
                                          alt="{{ $related->title }} - Back"
-                                         class="flip-card-back absolute w-full h-full object-cover rounded-2xl backface-hidden rotate-y-180 shadow-lg">
+                                         class="flip-card-back absolute w-full h-full object-cover rounded-2xl backface-hidden rotate-y-180 shadow-lg"
+                                         @click="modalOpen = true; selectedImage = '{{ asset('storage/' . $related->back_image) }}'">
                                 @endif
                             </div>
                             <div class="p-3 text-center mt-2">
@@ -72,38 +74,19 @@
             </div>
         @endif
 
-        {{-- نافذة تكبير الصور لجميع العملات --}}
-        {{-- نافذة تكبير الصور لجميع العملات --}}
+        {{-- نافذة تكبير الصور (مودال) --}}
         <div x-show="modalOpen" x-transition.opacity
              class="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50 p-4"
              @click="modalOpen = false">
 
-            <div @click.stop
-                 class="relative w-full max-w-[90vw] max-h-[90vh] perspective cursor-pointer flex items-center justify-center"
-                 x-data="{ modalBack: false }"
-                 :key="selectedCoin?.id">
-
-                <div class="flip-card-inner relative w-full h-full transition-transform duration-500 flex items-center justify-center"
-                     :class="{'rotate-y-180': modalBack}"
-                     @click="modalBack = !modalBack">
-
-                    <template x-if="selectedCoin?.image">
-                        <img :src="'/storage/' + selectedCoin.image"
-                             class="flip-card-front max-h-[90vh] max-w-[90vw] object-contain backface-hidden rounded-2xl shadow-2xl">
-                    </template>
-
-                    <template x-if="selectedCoin?.back_image">
-                        <img :src="'/storage/' + selectedCoin.back_image"
-                             class="flip-card-back max-h-[90vh] max-w-[90vw] object-contain backface-hidden rotate-y-180 rounded-2xl shadow-2xl">
-                    </template>
-                </div>
-
+            <div @click.stop class="relative max-w-5xl max-h-[90vh] flex items-center justify-center">
+                <img :src="selectedImage"
+                     class="max-h-[90vh] max-w-[90vw] object-contain rounded-2xl shadow-2xl" />
                 {{-- زر إغلاق --}}
                 <button @click="modalOpen = false"
                         class="absolute top-3 right-3 text-white text-4xl font-extrabold hover:text-gray-300">&times;</button>
             </div>
         </div>
-
 
     </div>
 
