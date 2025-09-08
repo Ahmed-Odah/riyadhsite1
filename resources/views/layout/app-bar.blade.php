46b73c5ac24fcf23dd3752090be2512af132a78e
ahmed <ahmedaoudah1@gmail.com>
2025-09-08 10:35:11 +0300

    d

--- resources/views/layout/app-bar.blade.php
+++ resources/views/layout/app-bar.blade.php
@@ -54,8 +54,9 @@
                 opacity-0 scale-95 invisible group-hover:opacity-100 group-hover:scale-100
                 group-hover:visible transition-all duration-200 z-10">
                     <a href="/whous" class="block px-4 py-2 hover:bg-gray-100">من آنا</a>
-                    <a href="{{route('official')}}" class="block px-4 py-2 hover:bg-gray-100">صور رسميه لي</a>
                     <a href="{{route('certificate')}}" class="block px-4 py-2 hover:bg-gray-100">الشهادات والدورات</a>
+
+                    <a href="{{route('official')}}" class="block px-4 py-2 hover:bg-gray-100">صور رسميه لي</a>
 
                 </div>
             </div>
