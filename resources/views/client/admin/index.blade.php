1f9a2027da2d7fad39139b1727080c3e4502db8d
ahmed <ahmedaoudah1@gmail.com>
2025-09-10 18:06:02 +0300

    d

--- resources/views/client/admin/index.blade.php
+++ resources/views/client/admin/index.blade.php
@@ -36,7 +36,7 @@
                                         حذف
                                     </button>
                                 </form>
-                                <form action="{{ route('client.edit', $client->id) }}" method="GET">
+                                <form action="{{ route('client-edit', $client->id) }}" method="GET">
                                     <button type="submit"
                                             class="px-3 py-1.5 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition text-sm font-medium">
                                         تعديل
