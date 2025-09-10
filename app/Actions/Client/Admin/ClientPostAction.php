547f9a279558b6024a014f27e0e2a4a9d1b330c6
ahmed <ahmedaoudah1@gmail.com>
2025-09-10 17:51:25 +0300

    d

--- app/Actions/Client/Admin/ClientPostAction.php
+++ app/Actions/Client/Admin/ClientPostAction.php
@@ -4,6 +4,7 @@ namespace App\Actions\Client\Admin;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class ClientPostAction
@@ -12,15 +13,15 @@ class ClientPostAction
 
     public function handle(Request $request)
     {
-        // إنشاء عميل جديد
+        // إنشاء عميل جديد مع كلمة المرور
         Client::create([
-            'name'    => $request->get('name'),
-            'email'   => $request->get('email'),
-            'phone'   => $request->get('phone'),
-            'address' => $request->get('address'),
+            'name'     => $request->get('name'),
+            'email'    => $request->get('email'),
+            'phone'    => $request->get('phone'),
+            'address'  => $request->get('address'),
+            'password' => Hash::make($request->get('password')),
         ]);
 
         return back(); // أو redirect لصفحة قائمة العملاء
     }
-
 }
