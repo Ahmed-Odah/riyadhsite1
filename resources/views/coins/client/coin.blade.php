32b109c516398f918db54db1d7b96e72bb314a70
ahmed <ahmedaoudah1@gmail.com>
2025-09-11 12:15:56 +0300

    d

--- resources/views/coins/client/coin.blade.php
+++ resources/views/coins/client/coin.blade.php
@@ -18,7 +18,7 @@
                     >
                         <button @click="open = true; image = @js(asset('storage/' . $coin->image))" class="block w-full">
                             <div class="aspect-[4/3] w-full overflow-hidden">
-                                <img src="{{ asset('storage/' . $coin->image) }}"
+                                <img src="{{ asset('public/storage/' . $coin->image) }}"
                                      alt="{{ $coin->title }}"
                                      class="w-full h-full object-cover transform group-hover:scale-105 transition duration-500" />
                             </div>
