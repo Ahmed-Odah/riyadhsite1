dd36689064dd389023d11af45ca5fb110651f355
ahmed <ahmedaoudah1@gmail.com>
2025-09-17 18:48:55 +0300

    d

--- database/migrations/2025_07_24_174606_create_blogs_table.php
+++ database/migrations/2025_07_24_174606_create_blogs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
-            $table->longText('description')->nullable(); // نص طويل
+            $table->string('description')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
         });
