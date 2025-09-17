257bfd2223f5b18d3809401c318a35316b107f8c
ahmed <ahmedaoudah1@gmail.com>
2025-09-17 18:48:40 +0300

    d

--- database/migrations/2025_09_17_154318_change_description_to_longtext_in_blogs_table.php
+++ database/migrations/2025_09_17_154318_change_description_to_longtext_in_blogs_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
-            $table->id();
-            $table->string('title')->nullable();
-            $table->longText('description')->nullable(); // نص طويل
-            $table->string('image')->nullable();
-            $table->timestamps();
+        Schema::table('blogs', function (Blueprint $table) {
+            //
         });
     }
 
@@ -25,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blogs');
+        Schema::table('blogs', function (Blueprint $table) {
+            //
+        });
     }
 };
