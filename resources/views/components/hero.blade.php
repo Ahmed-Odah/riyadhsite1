cb770a466f6c40911fe73695a828f86553108327
ahmed <ahmedaoudah1@gmail.com>
2025-09-18 11:02:05 +0300

    d

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -112,9 +112,10 @@
 
 <div class="bg-gradient text-white min-h-screen flex items-center justify-center relative overflow-hidden">
     <!-- الخلفية المتحركة -->
-    <div class="absolute inset-0 z-0">
-        <img src="/public/book2005.png" class="w-full h-full object-contain brightness-90" alt="Book Image">
+    <div class="absolute inset-0 flex justify-center items-center z-0">
+        <img src="/public/book2005.png" class="max-w-full max-h-full object-contain brightness-90" alt="Book Image">
     </div>
+
 
     <!-- النصوص -->
     <div class="container mx-auto px-6 text-center relative z-10">
