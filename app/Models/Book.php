cb497fa64f996da196f300b58dedacfb5698ab87
ahmed <ahmedaoudah1@gmail.com>
2025-09-22 16:43:30 +0300

    update

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,12 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $fillable = [
-        'title',
-        'description',
-        'content',
-        'cover_url',
-        'image',
-        'is_pending', // ✅ إضافة العمود الجديد
-    ];
+    protected $fillable = ['title', 'description', 'content', 'cover_url', 'image'];
 }
