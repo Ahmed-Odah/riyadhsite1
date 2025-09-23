f1609a9b4f97f32a4e91b320095bd180c7cf7a99
ahmed <ahmedaoudah1@gmail.com>
2025-09-23 17:53:26 +0300

    update

--- app/Actions/Books/client/BooksClientIndexAction.php
+++ app/Actions/Books/client/BooksClientIndexAction.php
@@ -12,10 +12,10 @@ class BooksClientIndexAction
     public function handle()
     {
         // أول 10 كتب منشورة
-        $books = Book::where('is_pending', 0)->take(10)->get();
+        $books = Book::where('is_pending', 0)->take(2)->get();
 
         // أول 10 كتب تحت الطباعة
-        $pendingBooks = Book::where('is_pending', 1)->take(10)->get();
+        $pendingBooks = Book::where('is_pending', 1)->take(2)->get();
 
         return view('books.client.index', compact('books', 'pendingBooks'));
     }
