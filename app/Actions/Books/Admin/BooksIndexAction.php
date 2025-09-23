3a1ffed35a9f3fa47cf7a0bdf8a544151ebc2df8
ahmed <ahmedaoudah1@gmail.com>
2025-09-23 17:55:02 +0300

    update

--- app/Actions/Books/Admin/BooksIndexAction.php
+++ app/Actions/Books/Admin/BooksIndexAction.php
@@ -11,13 +11,7 @@ class BooksIndexAction
 
     public function handle()
     {
-        // جميع الكتب المنشورة (is_pending = 0)
-        $books = Book::where('is_pending', 0)->get();
-
-        // جميع الكتب تحت الطباعة (is_pending = 1)
-        $pendingBooks = Book::where('is_pending', 1)->get();
-
-        // تمرير المتغيرات للـ View
-        return view('books.admin.index', compact('books', 'pendingBooks'));
+        $books = Book::all();
+        return view('books.admin.index', compact('books'));
     }
 }
