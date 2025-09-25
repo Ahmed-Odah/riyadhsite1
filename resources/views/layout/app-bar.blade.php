9fb4d0e8b5d1b0c023ff23c7daa404ee888056a8
ahmed <ahmedaoudah1@gmail.com>
2025-09-25 12:23:10 +0300

    update

--- resources/views/layout/app-bar.blade.php
+++ resources/views/layout/app-bar.blade.php
@@ -17,8 +17,8 @@
         </div>
 
         <!-- روابط سطح المكتب -->
-        <div class=" text-black hidden lg:flex flex-col md:flex-row items-center justify-center gap-8 md:static absolute w-full md:w-auto p-4 md:p-0 z-20 top-full left-0 md:top-auto md:left-auto"
-             :class="{'text-black': !scrolled, 'text-black': scrolled}">
+        <div class=" text-white hidden lg:flex flex-col md:flex-row items-center justify-center gap-8 md:static absolute w-full md:w-auto p-4 md:p-0 z-20 top-full left-0 md:top-auto md:left-auto"
+             :class="{'text-white': !scrolled, 'text-white': scrolled}">
             <style>
                 .nav-link {
                     @apply hover:no-underline transition-all duration-300 hover:text-gray-300 transform hover:scale-105;
@@ -206,7 +206,7 @@
     /* زر المنيو */
     #menu-toggle,
     #menu-toggle .material-icons {
-        color: white !important;
+        color: black !important;
     }
 
     /* عند النزول */
