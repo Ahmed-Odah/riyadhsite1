f3711401302319e7b4005ccb11832890ab5f990d
ahmed <ahmedaoudah1@gmail.com>
2025-10-05 19:49:45 +0300

    k

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -1,10 +1,8 @@
 @extends('layout.master')
 @section('content')
-    <div x-data="{ open: false, image: '', visible: 12 }" class="relative">
         <!-- العنوان -->
         <div class="bg-gray-50 min-h-screen py-16 px-4 sm:px-10 lg:px-20">
             <h1 class="text-3xl font-extrabold text-center text-gray-800 mt-20">الحمامات</h1>
 
-        </div>
     </div>
         >@endsection
