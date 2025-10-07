583059848848a8b93f1a825bf5ce4ff30bdecf56
ahmed <ahmedaoudah1@gmail.com>
2025-10-07 11:51:26 +0300

    k

--- resources/views/layout/app-bar.blade.php
+++ resources/views/layout/app-bar.blade.php
@@ -1,38 +1,13 @@
-<nav id="nav" class="fixed left-0 top-0 w-full px-5 py-3 bg-transparent  transition-colors duration-300">
+<nav id="nav" class="fixed top-0 w-full px-5 py-3 bg-transparent  transition-colors duration-300">
 
     <div class="flex justify-between items-center container mx-auto relative">
         <!-- اللوجو -->
-        <a href="" class="flex flex-col items-start gap-0 relative">
-            <div class="flex items-center gap-5 -ml-2 sm:-ml-4 md:-ml-6 lg:-ml-8 xl:-ml-10 relative">
+        <a href="" class="flex flex-col items-start gap-0">
+            <div class="flex items-center gap-5 -ml-2 sm:-ml-4 md:-ml-6 lg:-ml-8 xl:-ml-10">
                 <span id="logo-text" class="text-lg font-bold lg:text-3xl mt-1"></span>
-                <div class="relative w-32 h-32 flex items-center justify-center">
-                    <!-- الخط الدائري المتحرك -->
-                    <div class="absolute w-28 h-28 rounded-full border-4 border-yellow-500 animate-orbit"></div>
-                    <!-- الشعار -->
-                    <img src="{{ asset('/public/r2000.png') }}" alt="Logo" class="w-24 relative z-10">
-                </div>
+                <img src="{{ asset('/public/r2000.png') }}" alt="Logo" class="w-30">
             </div>
         </a>
-
-        <style>
-            /* حركة الخط الدائري حول الشعار */
-            @keyframes orbit {
-                0% {
-                    transform: rotate(0deg) translateX(4px) rotate(0deg);
-                }
-                50% {
-                    transform: rotate(180deg) translateX(4px) rotate(-180deg);
-                }
-                100% {
-                    transform: rotate(360deg) translateX(4px) rotate(-360deg);
-                }
-            }
-
-            .animate-orbit {
-                animation: orbit 4s linear infinite;
-            }
-        </style>
-
 
         <!-- زر المنيو للجوال -->
         <div class="lg:hidden absolute left-4 top-1/2 transform -translate-y-1/2">
@@ -45,7 +20,7 @@
         <div class="hidden lg:flex flex-col md:flex-row items-center justify-center gap-8 md:static absolute w-full md:w-auto p-4 md:p-0 z-20 top-full left-0 md:top-auto md:left-auto
     {{ request()->routeIs('homepage') ? 'text-white' : 'text-black' }}">
 
-        <style>
+            <style>
                 .nav-link {
                     @apply hover:no-underline transition-all duration-300 hover:text-gray-300 transform hover:scale-105;
                 }
