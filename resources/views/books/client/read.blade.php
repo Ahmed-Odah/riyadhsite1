91ef079f2dde3646fb198c00fc911fa643121046
ahmed <ahmedaoudah1@gmail.com>
2025-11-15 14:41:44 +0300

    k

--- resources/views/books/client/read.blade.php
+++ resources/views/books/client/read.blade.php
@@ -1,15 +1,17 @@
-    <div class="container mx-auto p-6">
-        <h1 class="text-2xl font-bold mb-4">{{ $book->title }}</h1>
+<div class="container mx-auto p-4">
+    <h1 class="text-xl md:text-2xl font-bold mb-4">{{ $book->title }}</h1>
 
-        <div class="rounded-xl overflow-hidden shadow-lg border">
-            <iframe
-                src="{{ route('books.pdf', $book->id) }}"
-                width="100%"
-                height="800px"
-                class="w-full">
-            </iframe>
-        </div>
+    <div class="rounded-xl overflow-hidden shadow-lg border h-[70vh] md:h-[800px]">
+        <iframe
+            src="{{ route('books.pdf', $book->id) }}"
+            class="w-full h-full border-0"
+            allowfullscreen
+            webkitallowfullscreen
+            mozallowfullscreen>
+        </iframe>
     </div>
+</div>
+
 
 
 
